540975711c4623c02066c00ac2cdfa1f9becb568
Kiah <kiah@notch8.com>
2020-06-24 18:21:00 -0700

    prettify json for human readability

--- spec/jobs/save_structure_job_spec.rb
+++ spec/jobs/save_structure_job_spec.rb
@@ -6,8 +6,8 @@ describe SaveStructureJob do
   let(:paged_resource) { FactoryBot.create(:paged_resource_with_one_image) }
   let(:structure) { File.read(Rails.root.join('spec', 'fixtures', 'structure.json')) }
 
-  context "save structure job" do
-    it "saves the structure successfully" do
+  context 'save structure job' do
+    it 'saves the structure successfully' do
       expect(paged_resource.logical_order.order).to be_empty
       described_class.perform_now(paged_resource, structure)
       expect(paged_resource.logical_order.order).to be
