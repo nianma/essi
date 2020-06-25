165b29dee4991029b1adaf80bbd9406d08664a4e
Kiah <kiah@notch8.com>
2020-06-25 12:33:18 -0700

    passing structure job spec

--- spec/jobs/save_structure_job_spec.rb
+++ spec/jobs/save_structure_job_spec.rb
@@ -1,16 +1,40 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'rails_helper'
 
-describe SaveStructureJob do
-
+describe SaveStructureJob do # rubocop:disable Metrics/BlockLength
   let(:paged_resource) { FactoryBot.create(:paged_resource_with_one_image) }
-  let(:structure) { File.read(Rails.root.join('spec', 'fixtures', 'structure.json')) }
+  let(:structure)      { build_structure }
 
-  context 'save structure job' do
+  describe '#perform' do
     it 'saves the structure successfully' do
       expect(paged_resource.logical_order.order).to be_empty
+
       described_class.perform_now(paged_resource, structure)
-      expect(paged_resource.logical_order.order).to be
+
+      expect(paged_resource.logical_order.order).to_not be_empty
+      expect(paged_resource.logical_order.order['label']).to eq('Logical')
+      expect(paged_resource.logical_order.order.dig('nodes').first.dig('label'))
+        .to eq('Main')
+      expect(paged_resource.logical_order.order.to_s)
+        .to include(paged_resource.file_set_ids.first.to_s)
     end
+  end
+
+  def build_structure # rubocop:disable Metrics/MethodLength
+    {
+      'label': 'Logical',
+      'nodes': [
+        {
+          'label': 'Main',
+          'nodes': [
+            {
+              'proxy': paged_resource.file_set_ids.first.to_s
+            }
+          ]
+        }
+      ]
+    }.to_json
   end
 end
