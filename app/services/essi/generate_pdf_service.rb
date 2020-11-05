ec883983ee6e76da730e8a37b6253e32a494c518
nianma <nianma@indiana.edu>
2020-11-05 00:25:48 -0500

    essi-980: Use IIIF to convert images when generating PDF files

--- app/services/essi/generate_pdf_service.rb
+++ app/services/essi/generate_pdf_service.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 module ESSI
   class GeneratePdfService
     # Generate PDFs for download
@@ -40,11 +42,27 @@ module ESSI
         Tempfile.create(fs.original_file.file_name.first, dir_path) do |file|
           page_size = [CoverPageGenerator::LETTER_WIDTH, CoverPageGenerator::LETTER_HEIGHT]
           file.binmode
-          file.write(fs.original_file.content)
+	  write_iiif_file(file, fs)
+		
           pdf.image(file, fit: page_size, position: :center, vposition: :center)
         end
         paginate_images(pdf, i)
       end
+    end
+
+    def write_iiif_file(file, fs)
+	#while using iiif_image_url_builder.call, both the following cases work,
+	#but you cannot set nil for the second parameter. 
+	#To make downloading work, you also need make changes to essi_config.example.yml, 
+	#you can either set cantaloupe iiif_server_url: http://localhost:3000/iiif/2/, 
+	#or totally comment out the configuration for cantaloupe.
+	#One thing need to investigate is, although it shows the downloading completed in the log,
+	#but the browser has time out error when downloading at the first time,
+	#you have to refresh to view the downloading pdfs. .
+
+	url=Hyrax.config.iiif_image_url_builder.call(fs.original_file.id, "/", '800,')
+	#url=Hyrax.config.iiif_image_url_builder.call(fs.original_file.id, "localhost:3000/", '800,')
+	file.write open(url).read
     end
 
     def paginate_images(pdf, i)
